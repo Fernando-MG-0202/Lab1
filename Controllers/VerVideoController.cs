344150c848ffce2f415674584e54da1063343a6f
Fernando Enrique Mascorro Garcia <enriquemascorrogarcia@gmail.com>
2020-02-26 11:02:55 -0600

    Avanzado

---
+++ Controllers/VerVideoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VerVideoController : Controller
+    {
+        //
+        // GET: /VerVideo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
